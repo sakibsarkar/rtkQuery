4f9edfa21202e2c2e6d8fde7d6590bc25a2e49a1
sakibsarkar <sakibsarkar707@gmail.com>
2024-04-07 14:41:38 +0600

    add product system added

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useAddProductMutation } from "../../redux/features/api/apiSlice";
 
 const AddProduct = () => {
+  const [addProduct] = useAddProductMutation();
 
-    const [addProduct] = usead
+  //   add pruduct
+  const handleAddProduct = () => {
+    const productData = {
+      title: "test product",
+      price: 13.5,
+      description: "lorem ipsum set",
+      image: "https://i.pravatar.cc",
+      category: "electronic",
+    };
+    addProduct(productData);
+  };
 
   return (
     <div className="w-full h-full flex justify-center items-center gap-[20px] flex-col">
       <p>This is add product page</p>
-      <button className="btn"> Add a new product</button>
+      <button className="btn" onClick={handleAddProduct}>
+        {" "}
+        Add a new product
+      </button>
     </div>
   );
 };
